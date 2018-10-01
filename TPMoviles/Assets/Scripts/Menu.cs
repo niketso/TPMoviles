84cf93985feb2ccd415d13d6e5ee1fd393d1e3b8
Unknown <nicolaspiccitto@gmail.com>
2018-10-01 11:51:13 -0300

    Menu Principal y de derrota.

---
+++ TPMoviles/Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{    
+    public void ChangeLevel()
+    {
+        SceneManager.LoadScene("Prototipo");
+    }
+
+    public void StartScreen()
+    {
+        SceneManager.LoadScene("StartScreen");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
