80dcc52a7d778741dd8367b0748e8b2ae40f89a1
Unknown <li_gueva@hotmail.com>
2018-10-01 13:32:45 -0300

    Nivel Prototipo v0.3
    arreglado animator con mas animaciones y agregado score

---
+++ TPMoviles/Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UpdateScore : MonoBehaviour {
+	[SerializeField] string nextLevel;
+    //private Destructor score;
+    public Text scoreText;
+
+	[SerializeField] public int maxScore;
+    private int score;
+
+	public int Score{
+		get{ return score;}
+		set{score = value;}
+	}
+
+	void Start () {	
+		//score = GameObject.FindGameObjectWithTag ("Bullet").GetComponent<Destructor> ();
+	}
+
+	void Update(){
+		
+		if (score == maxScore)
+			SceneManager.LoadScene (nextLevel);
+		
+	}
+}
