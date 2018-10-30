bee1a85554fe61c3747a0c81be782a478b2e2a80
Unknown <li_gueva@hotmail.com>
2018-10-30 09:49:15 -0300

    Rays

--- TPMoviles/Assets/RayCreator.cs
+++ TPMoviles/Assets/RayCreator.cs
@@ -6,54 +6,61 @@ public class RayCreator : MonoBehaviour {
 
     Animator animatorEnemy;
     EnemyMovement enemy;
-    LayerMask layermaskEnemy = 10;
+    LayerMask layermaskEnemy = ~10;
+    Vector3 rayPos;
 
 	void Start ()
     {
         animatorEnemy = GetComponent<Animator>();
         enemy = GetComponent<EnemyMovement>();
-	}
+        rayPos = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
+
+    }
 	
 	void Update ()
     {
         
         RaycastHit hit1;
         var direction1 = transform.forward;
-        Physics.Raycast(transform.position, direction1, out hit1, 3);
+      //  Physics.Raycast(transform.position, direction1, out hit1, 3,layermaskEnemy);
 
         RaycastHit hit2;
-        var direction2 = Quaternion.AngleAxis(30, transform.up)*transform.forward;
-        Physics.Raycast(transform.position, direction2, out hit2, 3);
+        var direction2 = Quaternion.AngleAxis(15, transform.up)*transform.forward;
+       // Physics.Raycast(transform.position, direction2, out hit2, 3, layermaskEnemy);
 
         RaycastHit hit3;
-        var direction3 = Quaternion.AngleAxis(60, transform.up) * transform.forward;
-        Physics.Raycast(transform.position, direction3, out hit3, 3);
+        var direction3 = Quaternion.AngleAxis(30, transform.up) * transform.forward;
+       // Physics.Raycast(transform.position, direction3, out hit3, 3, layermaskEnemy);
 
         RaycastHit hit4;
-        var direction4 = Quaternion.AngleAxis(90, transform.up) * transform.forward;
-        Physics.Raycast(transform.position, direction4, out hit4, 3);
+        var direction4 = Quaternion.AngleAxis(45, transform.up) * transform.forward;
+       // Physics.Raycast(transform.position, direction4, out hit4, 3, layermaskEnemy);
 
         RaycastHit hit5;
-        var direction5 = Quaternion.AngleAxis(-30, transform.up) * transform.forward;
-        Physics.Raycast(transform.position, direction5, out hit5, 3);
+        var direction5 = Quaternion.AngleAxis(-15, transform.up) * transform.forward;
+       // Physics.Raycast(transform.position, direction5, out hit5, 3, layermaskEnemy);
 
         RaycastHit hit6;
-        var direction6 = Quaternion.AngleAxis(-60, transform.up) * transform.forward;
-        Physics.Raycast(transform.position, direction6, out hit6, 3);
+        var direction6 = Quaternion.AngleAxis(-30, transform.up) * transform.forward;
+        //Physics.Raycast(transform.position, direction6, out hit6, 3, layermaskEnemy);
 
         RaycastHit hit7;
-        var direction7 = Quaternion.AngleAxis(-90, transform.up) * transform.forward;        
-        Physics.Raycast(transform.position, direction7, out hit7, 3);
+        var direction7 = Quaternion.AngleAxis(-45, transform.up) * transform.forward;        
+        //Physics.Raycast(transform.position, direction7, out hit7, 3, layermaskEnemy);
 
-        if (hit1.transform.CompareTag("Enemy") || hit2.transform.CompareTag("Enemy") || hit3.transform.CompareTag("Enemy") || hit4.transform.CompareTag("Enemy")
-            || hit5.transform.CompareTag("Enemy") || hit6.transform.CompareTag("Enemy") || hit7.transform.CompareTag("Enemy"))
+        if(Physics.Raycast(rayPos, direction1, out hit1, 3, layermaskEnemy) || Physics.Raycast(rayPos, direction2, out hit2, 3, layermaskEnemy) || 
+           Physics.Raycast(rayPos, direction3, out hit3, 3, layermaskEnemy) || Physics.Raycast(rayPos, direction4, out hit4, 3, layermaskEnemy) || 
+           Physics.Raycast(rayPos, direction5, out hit5, 3, layermaskEnemy) || Physics.Raycast(rayPos, direction6, out hit6, 3, layermaskEnemy) || 
+           Physics.Raycast(rayPos, direction7, out hit7, 3, layermaskEnemy))
         {
+            Debug.DrawRay(transform.position, transform.forward, Color.green);
             Debug.Log("CHOQUE CONTRA ENEMIGO");
             animatorEnemy.SetFloat("Speed", 0);
             enemy.isStopped = true;
         }
         else
         {
+            Debug.DrawRay(transform.position, transform.forward, Color.green);
             animatorEnemy.SetFloat("Speed", 0.1f);
             enemy.isStopped = false;
         }
